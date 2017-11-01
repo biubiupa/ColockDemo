84fa66fbdaade1e8b3c6a06870068874c9b30ed1
dorayo <yomingyo@gmail.com>
2017-11-01 14:48:33 +0800

    lasttime

--- TimeColock/ViewController.m
+++ TimeColock/ViewController.m
@@ -16,20 +16,43 @@
 
 @implementation ViewController
 
+
+
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSCalendar *calendar=[NSCalendar currentCalendar];
-    NSTimeZone *timeZone=[NSTimeZone localTimeZone];
-    NSDate *currentdate=[NSDate date];
-    [calendar setTimeZone:timeZone];
-    NSDateComponents *currentTime=[calendar components:NSCalendarUnitSecond|NSCalendarUnitMinute|NSCalendarUnitHour|NSCalendarUnitTimeZone fromDate:currentdate];
+    
 //设置表盘
-    UIImageView *imageView=[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"clock"]];
-    imageView.bounds=CGRectMake(0, 0, 300, 300);
-    imageView.center=self.view.center;
-    [self.view addSubview:imageView];
-//秒针
+//    UIImageView *imageView=[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"clock"]];
+//    imageView.bounds=CGRectMake(0, 0, 300, 300);
+//    imageView.center=self.view.center;
+//    [self.view addSubview:imageView];
+    CALayer *layer=[[CALayer alloc] init];
+    layer.bounds=CGRectMake(0, 0, 300, 300);
+    layer.position=self.view.center;
+    layer.contents=(__bridge id _Nullable)[UIImage imageNamed:@"clock"].CGImage;
+    [self.view.layer addSublayer:layer];
+
+//时针
+    UIView * hourView =[[UIView alloc] init];
+    hourView.backgroundColor=[UIColor grayColor];
+    hourView.bounds=CGRectMake(0, 0, 3, 80);
+    hourView.center=self.view.center;
+    hourView.layer.anchorPoint=CGPointMake(0.5, 1);
+    self.hourView=hourView;
+    [self.view addSubview:hourView];
+    //分针
+    UIView *minuteView=[[UIView alloc] init];
+    minuteView.backgroundColor=[UIColor lightGrayColor];
+    minuteView.bounds=CGRectMake(0, 0, 2, 100);
+    minuteView.center=self.view.center;
+    minuteView.layer.anchorPoint=CGPointMake(0.5, 1);
+    self.minuteView=minuteView;
+    [self.view addSubview:minuteView];
+    //秒针
     UIView *secondView=[[UIView alloc] init];
     secondView.backgroundColor=[UIColor redColor];
     secondView.bounds=CGRectMake(0, 0, 1, 120);
@@ -38,13 +61,29 @@
     secondView.layer.anchorPoint=CGPointMake(0.5, 1);
     self.secondView=secondView;
     [self.view addSubview:secondView];
+
+//创建CADisplayLink
+    CADisplayLink *link=[CADisplayLink displayLinkWithTarget:self selector:@selector(transformAngel)];
+    [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
 }
 
+- (void)transformAngel {
+    NSCalendar *calendar=[NSCalendar currentCalendar];
+    NSTimeZone *timeZone=[NSTimeZone localTimeZone];
+    NSDate *currentdate=[NSDate date];
+    [calendar setTimeZone:timeZone];
+    NSDateComponents *currentTime=[calendar components:NSCalendarUnitSecond|NSCalendarUnitMinute|NSCalendarUnitHour|NSCalendarUnitTimeZone fromDate:currentdate];
+    
+    CGFloat secondAngel=(M_PI * 2 / 60) *currentTime.second;
+    self.secondView.transform=CGAffineTransformMakeRotation(secondAngel);
+    CGFloat minuteAngel=(M_PI * 2 / 60) *currentTime.minute;
+    self.minuteView.transform=CGAffineTransformMakeRotation(minuteAngel);
+    CGFloat hourAngel=(M_PI * 2 / 12) *currentTime.hour;
+    self.hourView.transform=CGAffineTransformMakeRotation(hourAngel);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
 @end
